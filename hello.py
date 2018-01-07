d1057c6e575edb0b7f423d21a25c30375707ab2c
Braden Fase <bradennnn@brefend.com>
2018-01-07 09:56:35 -0500

    Updated hello.py with different example

--- hello.py
+++ hello.py
@@ -3,13 +3,24 @@
 # import modules used here -- sys is a very standard one
 import sys
 
+def repeat(s, exclaim):
+	"""
+	This is an example of a function docstring.
+	Triple Quotes are a feature unique to python.
+	"""
+	# Variables defined in a function are local to that function.
+	result = s * 3 # or s+s+s. * 3 is also faster due to the fact it's only one calculation.
+	if exclaim:
+		result = result + '!!!'
+	return result
+
 # Gather our code in a main() function
 def main():
-    print 'Hello there', sys.argv[1]
-    # Command line args are in sys.argv[1], sys.argv[2] ...
-    # sys.argv[0] is the script name itself and can be ignored
-
+	print repeat('Yay', False)
+	print repeat('Woo Hoo', True)
 # Standard boilerplate to call the main() function to begin
 # the program.
+'''
 if __name__ == '__main__':
     main()
+'''
