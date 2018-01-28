f14bcccf4e99b09d6f32f882bbc3c1e9edfafa76
brafas <braden@brefend.com>
2018-01-28 14:00:00 -0500

    Update

--- pyprac/bs4 prac.py
+++ pyprac/bs4 prac.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-from unicodedata import uni
+
 page = requests.get("https://www.etsy.com/search?q=furry&explicit=1&order=date_desc")
 # page.raise_for_status() # raises an exception if the can't load it.
 page_soup = BeautifulSoup(page.text, "html.parser")
 listing_card = page_soup.find_all("div", class_="v2-listing-card__info")
 
-print(listing_card[0].p)
-unicode()
+# print(listing_card[0].p)
+print(str(listing_card[0].p.string))
